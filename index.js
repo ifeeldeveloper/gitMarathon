bdb0576d9b76e713d50a0329c94f8b7e62614b80
Jagadish Bhatta <jagadishbhatta16@gmail.com>
2024-02-11 11:58:57 +0545

    update JS file

--- index.js
+++ index.js
@@ -7,5 +7,28 @@ var square2 = square(n);
 var square4 = square(4);
 console.log(square2);
 console.log(square4);
-alert(square2);
-alert(square4);
+
+
+var x=7;
+function getName(){
+  console.log('Hello JavaScript')
+}
+getName();
+console.log(x);
+
+
+
+var y=1;
+a();
+b();
+console.log(y);
+
+function a(){
+  var y=10
+  console.log(y);
+}
+
+function b(){
+  var y=100;
+  console.log(y)
+}
